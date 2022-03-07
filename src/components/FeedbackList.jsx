cd526a93d87c6ab67f6a4cebc437b3530818ba27
Josh Wilensky <joshwilensky@gmail.com>
2022-03-07 11:10:52 -0500

    Updated Feedback List - deleted spinner

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,7 +1,6 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useContext } from "react";
 import FeedbackItem from "./FeedbackItem";
-import Spinner from "./shared/Spinner";
 import FeedbackContext from "../context/FeedbackContext";
 
 function FeedbackList() {
